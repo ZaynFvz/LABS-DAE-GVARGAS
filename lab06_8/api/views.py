4bfca77d7ab7f3a70716ec3d56cfdd1de8359c66
Gonzalo Vargas <gonzalo.vargas@tecsup.edu.pe>
2023-05-14 03:27:17 -0500

    Laboratorio 08 - Parte 2

---
+++ lab06_8/api/views.py
@@ -0,0 +1,31 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import viewsets
+
+from tienda.models import Categoria,Producto
+from .serializers import(CategoriaSerializer, ProductoSerializer)
+
+# Mostrar las categorias de nuestra tienda
+class IndexView(APIView):
+    
+    def get(self,request):
+        lista_categorias  = Categoria.objects.all()
+        serializer_categoria = CategoriaSerializer(lista_categorias, many=True)
+        return Response(serializer_categoria.data)
+
+#Mostrar las categorias de nuestra tienda incluyendo metodos POST y GET para añadir nuevos elementos 
+class CategoriaView(generics.ListCreateAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+#Métodos para eliminar y editar las categorias
+class CategoriaDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Categoria.objects.all()
+    lookup_url_kwarg = 'categoria_id'
+    serializer_class = CategoriaSerializer
+
+class ProductoViewSet(viewsets.ModelViewSet):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+
